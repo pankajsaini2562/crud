44f2e256839f510be9d33a141372994549d5fdc2
pankajvidyainternahub <pankaj.s@vidyainternahub.onmicrosoft.com>
2025-01-02 14:47:47 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,12 @@ const __dirname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 app.listen(4000, () => {
   console.log("server is running succesfully");
 });
 app.use("/user", todoRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
